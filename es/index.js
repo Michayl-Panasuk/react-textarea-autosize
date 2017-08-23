07ee3b1022088846c05b215d1fd11983b94a6883
MiAnn <pan-misha1@yandex.ru>
2017-08-23 22:14:04 +0300

    fix ie10/ie11 error

--- es/index.js
+++ es/index.js
@@ -1,11 +1,5 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('react'), require('prop-types')) :
-	typeof define === 'function' && define.amd ? define(['react', 'prop-types'], factory) :
-	(global.TextareaAutosize = factory(global.React,global.PropTypes));
-}(this, (function (React,PropTypes) { 'use strict';
-
-React = React && React.hasOwnProperty('default') ? React['default'] : React;
-PropTypes = PropTypes && PropTypes.hasOwnProperty('default') ? PropTypes['default'] : PropTypes;
+import React from 'react';
+import PropTypes from 'prop-types';
 
 var isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined';
 
@@ -397,6 +391,4 @@ TextareaAutosize.defaultProps = {
   useCacheForDOMMeasurements: false
 };
 
-return TextareaAutosize;
-
-})));
+export default TextareaAutosize;
